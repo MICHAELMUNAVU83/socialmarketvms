c98ae607d258bdaabb052fee08c2d6be46a3ed32
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2025-02-19 14:10:21 +0300

    Added

--- lib/socialmarketvms_web/components/core_components.ex
+++ lib/socialmarketvms_web/components/core_components.ex
@@ -558,7 +558,7 @@ defmodule SocialmarketvmsWeb.CoreComponents do
 
   def back(assigns) do
     ~H"""
-    <div class="mt-16">
+    <div class="mt-2">
       <.link
         navigate={@navigate}
         class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
--- lib/socialmarketvms_web/live/event_live/categories.ex
+++ lib/socialmarketvms_web/live/event_live/categories.ex
@@ -1,4 +1,4 @@
-defmodule SocialmarketvmsWeb.EventLive.Show do
+defmodule SocialmarketvmsWeb.EventLive.Categories do
   use SocialmarketvmsWeb, :system_live_view
 
   alias Socialmarketvms.Events
@@ -14,10 +14,13 @@ defmodule SocialmarketvmsWeb.EventLive.Show do
   def handle_params(%{"id" => id}, _, socket) do
     {:noreply,
      socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:page_title, "Categores")
      |> assign(:event, Events.get_event!(id))}
   end
 
-  defp page_title(:show), do: "Show Event"
-  defp page_title(:edit), do: "Edit Event"
+  def render(assigns) do
+    ~H"""
+    <div></div>
+    """
+  end
 end
